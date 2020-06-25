731dd15ba88e0f7d7eb6dd0ac5590c62cc6038c1
Nat <psalmnat@gmail.com>
2020-06-25 13:40:00 +0000

    events handling and binding handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,42 +1,49 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
-  state = {   
-      count:0,
-      tags:[]
+  state = {
+    count: 0,
+    tags: [],
   };
-//   styles = {
-//       fontSize: 10, 
-//       fontWeight: 'bold'
-//   }
- 
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
+  constructor(){
+      super();
+      // bind handle increment function
+    //  this.handleIncrement =   this.handleIncrement.bind(this);
+  }
+
+  handleIncrement = () => {
+    console.log("clicked");
+  }
 
   render() {
-     
     return (
       <>
-       
-        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button> */}
-        <ul>
-            {this.state.tags.length === 0 && "Please add New Tags"}
-       {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-        </ul>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </>
     );
   }
 
-//   getBadgeClasses() {
-//         let classes = "badge m-2 badge-";
-//         classes += (this.state.count === 0) ? "warning" : "primary";
-//         return classes;
-//     }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-//   formatCount() {
-//       const {count} = this.state
-//       return count === 0? 'Zero' : count;
-//   }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
