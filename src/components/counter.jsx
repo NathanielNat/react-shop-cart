f2f5bb2a613a89c6b03b52c876b44cceefad4c15
Nat <psalmnat@gmail.com>
2020-06-25 14:51:56 +0000

    passing arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,49 +2,44 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: [],
+    value: this.props.value
+    
   };
   styles = {
     fontSize: 10,
     fontWeight: "bold",
   };
 
-  constructor(){
-      super();
-      // bind handle increment function
-    //  this.handleIncrement =   this.handleIncrement.bind(this);
-  }
+  
 
   handleIncrement = () => {
-    this.setState({
-        count: this.state.count+1
-    })
+    this.setState({ value: this.state.value+1 });
   }
 
   render() {
+  
     return (
-      <>
+       
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
-          className="btn btn-secondary btn-sm"
-        >
+          className="btn btn-secondary btn-sm">
           Increment
         </button>
-      </>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
